e5faca963bfbd77fc8e08b9f8bb7b3bc7ad5b060
GabrieleNunez <g_m_nunez@live.com>
2019-07-08 13:28:13 -0700

    * Added files array into package.json * Added prettier into pipeline * Added readme * Added database driver based off sequelize * Created basic process model

---
+++ src/drivers/database.ts
@@ -0,0 +1,54 @@
+import * as Sequelize from 'sequelize';
+
+/**
+ * A complete set of options for our database driver to use
+ */
+export interface DatabaseOptions {
+    login: {
+        database: string;
+        username: string;
+        password: string;
+    };
+    orm: Sequelize.Options;
+}
+
+/**
+ * A class that starts and wraps basic database functionality using Sequelize
+ */
+export class Database {
+    private db: Sequelize.Sequelize;
+
+    /**
+     * Construct the basic datbaase object and initialize a connection
+     */
+    public constructor(ormOptions: DatabaseOptions) {
+        this.db = new Sequelize.Sequelize(
+            ormOptions.login.database,
+            ormOptions.login.username,
+            ormOptions.login.password,
+            ormOptions.orm,
+        );
+    }
+
+    /**
+     * Return the sequelize connection directly
+     */
+    public connection(): Sequelize.Sequelize {
+        return this.db;
+    }
+
+    /**
+     * From what has been initialized already, pull the model from the database object
+     * This can work across multiple modules at runtime
+     * @param modelName The name of the model specified by the factory
+     */
+    public model<T1 extends typeof Sequelize.Model>(modelName: string): T1 {
+        if (typeof this.db.models[modelName] !== 'undefined') {
+            return this.db.models[modelName] as T1;
+        } else {
+            throw 'Model: ' + modelName + ' not  found';
+        }
+    }
+}
+
+export default Database;
---
+++ src/models/process.ts
@@ -0,0 +1,57 @@
+import * as Sequelize from 'sequelize';
+
+/**
+ * The model that directly relates to the processes table
+ */
+export class Process extends Sequelize.Model {
+    public id!: number;
+    public name!: number;
+    public createdAt!: number;
+    public updatedAt!: number;
+
+    /**
+     * All associations that should be made to the process table
+     */
+    public static associations: {};
+}
+
+/**
+ * Column definitions for the table processes
+ */
+export const ProcessAttributesDefinition: Sequelize.ModelAttributes = {
+    id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+    },
+    name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        defaultValue: '',
+    },
+    createdAt: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    },
+    updatedAt: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    },
+};
+
+/**
+ * Factory that will simplfy initiating the process and making any additional associations
+ */
+export abstract class ProcessFactory {
+    public static init(sequelize: Sequelize.Sequelize): void {
+        Process.init(ProcessAttributesDefinition, {
+            sequelize: sequelize,
+            modelName: 'process',
+            tableName: 'processes',
+        });
+    }
+}
+
+export default ProcessFactory;
